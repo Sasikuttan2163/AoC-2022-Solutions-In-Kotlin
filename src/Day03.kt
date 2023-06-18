cd0d222d8f41ceecce12ef8472da30893f2400c8
Abhinav Anil <abhinav.anil.2k5@gmail.com>
2023-06-18 18:51:09 +0530

    fix: replaced unnecessary toHashSet calls with toSet

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,12 +1,12 @@
 fun main() {
     val input = readInput("Day03")
     val part1 = input.sumOf { l ->
-        l.substring(0, l.length / 2).toHashSet().intersect(l.substring(l.length / 2).toHashSet()).first().let {
+        l.substring(0, l.length / 2).toSet().intersect(l.substring(l.length / 2).toSet()).first().let {
             if (it.isLowerCase()) it - 'a' + 1 else it - 'A' + 27
         }
     }
     val part2 = input.chunked(3).sumOf { list ->
-        list[0].toHashSet().intersect(list[1].toHashSet()).intersect(list[2].toHashSet()).first().let {
+        list[0].toSet().intersect(list[1].toSet()).intersect(list[2].toSet()).first().let {
             if (it.isLowerCase()) it - 'a' + 1 else it - 'A' + 27
         }
     }
