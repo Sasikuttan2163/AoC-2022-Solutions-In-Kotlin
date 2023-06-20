153b579f1264b3feb490140d9d38dbd39ab42cf6
Abhinav Anil <abhinav.anil.2k5@gmail.com>
2023-06-20 22:29:50 +0530

    feat: updated day 3 intersect with infix notation

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,14 +1,18 @@
 fun main() {
     val input = readInput("Day03")
     val part1 = input.sumOf { l ->
-        l.substring(0, l.length / 2).toSet().intersect(l.substring(l.length / 2).toSet()).first().let {
-            if (it.isLowerCase()) it - 'a' + 1 else it - 'A' + 27
-        }
+        (l.substring(0, l.length / 2).toSet() intersect l.substring(l.length / 2).toSet())
+            .first()
+            .let {
+                if (it.isLowerCase()) it - 'a' + 1 else it - 'A' + 27
+            }
     }
     val part2 = input.chunked(3).sumOf { list ->
-        list[0].toSet().intersect(list[1].toSet()).intersect(list[2].toSet()).first().let {
-            if (it.isLowerCase()) it - 'a' + 1 else it - 'A' + 27
-        }
+        (list[0].toSet() intersect list[1].toSet() intersect list[2].toSet())
+            .first()
+            .let {
+                if (it.isLowerCase()) it - 'a' + 1 else it - 'A' + 27
+            }
     }
     part1.println()
     part2.println()
